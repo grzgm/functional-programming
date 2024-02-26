94fbba7c08bdffe2264f64b327ad3e72969cc81f
grzgm <g.malisz@student.fontys.nl>
2024-02-26 14:10:07 +0100

    pythagoras2 done

--- src/Pythagoras2.elm
+++ src/Pythagoras2.elm
@@ -1,8 +1,44 @@
 module Pythagoras2 exposing (..)
 
 import Html exposing (text)
+import List exposing (filter, map)
 import Pythagoras exposing (isTripleTuple, pythTriple)
 
 
 main =
-    text "Pythagoras2"
+    -- text <| Debug.toString <| pythTriplesRec [(5,4),(2,1),(35,7)]
+    text <| Debug.toString <| arePythTriplesRec [(1,2,3), (9,40,41), (3,4,5), (100,2,500)]
+
+
+pythTriplesMap : List ( Int, Int ) -> List ( Int, Int, Int )
+pythTriplesMap list =
+    map pythTriple list
+
+
+pythTriplesRec : List ( Int, Int ) -> List ( Int, Int, Int )
+pythTriplesRec list =
+    case list of
+        [] ->
+            []
+
+        x :: xs ->
+            pythTriple x :: pythTriplesRec xs
+
+
+arePythTriplesFilter : List ( Int, Int, Int ) -> List ( Int, Int, Int )
+arePythTriplesFilter list =
+    filter isTripleTuple list
+
+
+arePythTriplesRec : List ( Int, Int, Int ) -> List ( Int, Int, Int )
+arePythTriplesRec list =
+    case list of
+        [] ->
+            []
+
+        x :: xs ->
+            if isTripleTuple x then
+                x :: arePythTriplesRec xs
+
+            else
+                arePythTriplesRec xs
