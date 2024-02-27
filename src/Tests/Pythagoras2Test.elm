720e41779fdb9bcdd8042a9d2c09c3235c914fc2
grzgm <g.malisz@student.fontys.nl>
2024-02-27 16:25:55 +0100

    pythagoras 2 syntax update

--- src/Tests/Pythagoras2Test.elm
+++ src/Tests/Pythagoras2Test.elm
@@ -1,27 +1,45 @@
 module Tests.PythagorasTest exposing (..)
 
-import Pythagoras2 exposing (..)
 import Html exposing (text)
+import Pythagoras2 exposing (..)
 
 
-pythTriplesMapTest = [
-    toString <| pythTriplesMap [(5,4),(2,1),(35,7)] == [(9,40,41),(3,4,5),(1176,490,1274)],
-    toString <| pythTriplesMap [] == []]
+toString =
+    Debug.toString
 
-pythTriplesRecTest = [
-    toString <| pythTriplesRec [(5,4),(2,1),(35,7)] == [(9,40,41),(3,4,5),(1176,490,1274)],
-    toString <| pythTriplesRec [] == []]
 
-arePythTriplesFilterTest = [
-    toString <| arePythTriplesFilter [(1,2,3), (9,40,41), (3,4,5), (100,2,500)] == [(9,40,41),(3,4,5)],
-    toString <| arePythTriplesFilter [] == []]
+pythTriplesMapTest =
+    [ toString <| pythTriplesMap [ ( 5, 4 ), ( 2, 1 ), ( 35, 7 ) ] == [ ( 9, 40, 41 ), ( 3, 4, 5 ), ( 1176, 490, 1274 ) ]
+    , toString <| pythTriplesMap [] == []
+    ]
 
-arePythTriplesRecTest = [
-    toString <| arePythTriplesRec [(1,2,3), (9,40,41), (3,4,5), (100,2,500)] == [(9,40,41),(3,4,5)],
-    toString <| arePythTriplesRec [] == []]
 
-main = text(
-    "pythTriplesMapTest:" ++ String.concat pythTriplesMapTest ++ 
-    "pythTriplesRecTest:" ++ String.concat pythTriplesRecTest ++
-    "arePythTriplesFilterTest:" ++ String.concat arePythTriplesFilterTest ++ 
-    "arePythTriplesRecTest:" ++ String.concat arePythTriplesRecTest)
+pythTriplesRecTest =
+    [ toString <| pythTriplesRec [ ( 5, 4 ), ( 2, 1 ), ( 35, 7 ) ] == [ ( 9, 40, 41 ), ( 3, 4, 5 ), ( 1176, 490, 1274 ) ]
+    , toString <| pythTriplesRec [] == []
+    ]
+
+
+arePythTriplesFilterTest =
+    [ toString <| arePythTriplesFilter [ ( 1, 2, 3 ), ( 9, 40, 41 ), ( 3, 4, 5 ), ( 100, 2, 500 ) ] == [ ( 9, 40, 41 ), ( 3, 4, 5 ) ]
+    , toString <| arePythTriplesFilter [] == []
+    ]
+
+
+arePythTriplesRecTest =
+    [ toString <| arePythTriplesRec [ ( 1, 2, 3 ), ( 9, 40, 41 ), ( 3, 4, 5 ), ( 100, 2, 500 ) ] == [ ( 9, 40, 41 ), ( 3, 4, 5 ) ]
+    , toString <| arePythTriplesRec [] == []
+    ]
+
+
+main =
+    text
+        ("pythTriplesMapTest:"
+            ++ String.concat pythTriplesMapTest
+            ++ "pythTriplesRecTest:"
+            ++ String.concat pythTriplesRecTest
+            ++ "arePythTriplesFilterTest:"
+            ++ String.concat arePythTriplesFilterTest
+            ++ "arePythTriplesRecTest:"
+            ++ String.concat arePythTriplesRecTest
+        )
