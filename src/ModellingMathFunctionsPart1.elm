8017bcf321d8cec4f44ba9b5070aeeecf46f9605
grzgm <g.malisz@student.fontys.nl>
2024-03-03 20:25:07 +0100

    evaluating function value

--- src/ModellingMathFunctionsPart1.elm
+++ src/ModellingMathFunctionsPart1.elm
@@ -31,7 +31,25 @@ print fun =
     X ->
       "x"
       
+eval: Float -> Function -> Float
+eval num fun =
+  case fun of
+    Plus f1 f2 ->
+      (eval num f1) + (eval num f2)
+    Minus f1 f2 ->
+      (eval num f1) - (eval num f2)
+    Mult f1 f2 ->
+      (eval num f1) * (eval num f2)
+    Div f1 f2 ->
+      (eval num f1) / (eval num f2)
+    Poly f1 i ->
+      ((eval num f1) ^ (toFloat i))
+    Const i ->
+      (toFloat i)
+    X ->
+      num
+      
 f = Plus (Mult (Plus (Const 3) X) (Minus X (Poly X 5))) (Const 2)
 
 main =
-  text <| print f
+  text <| Debug.toString <| eval 1.0 f
